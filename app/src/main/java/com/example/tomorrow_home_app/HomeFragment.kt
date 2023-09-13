e84b41518647a5be0ff4dfe931983747b4013978
bjl0615 <bjl0615@gmail.com>
2023-09-14 01:36:30 +0900

    tomorrow_home_app FireStore DB Add

--- app/src/main/java/com/example/tomorrow_home_app/HomeFragment.kt
+++ app/src/main/java/com/example/tomorrow_home_app/HomeFragment.kt
@@ -1,12 +1,29 @@
 package com.example.tomorrow_home_app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.tomorrow_home_app.data.ArticleModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val db = Firebase.firestore
+
+        db.collection("articles").document("smsA64FiRARga5MOg6gO")
+            .get()
+            .addOnSuccessListener {result ->
+                val article = result.toObject<ArticleModel>()
+                Log.e("homeFragment" , article.toString())
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+            }
     }
 }
---
+++ app/src/main/java/com/example/tomorrow_home_app/data/ArticleModel.kt
@@ -0,0 +1,8 @@
+package com.example.tomorrow_home_app.data
+
+data class ArticleModel (
+    val articleID : String? = null,
+    val createdAt : Long? = null,
+    val description : String? = null,
+    val imageUrl : String? = null
+        )
