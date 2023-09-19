79ebebbc274bdd74420717a99171b0fed189fc67
bjl0615 <bjl0615@gmail.com>
2023-09-19 17:15:11 +0900

    tomorrow_home_app Write Page implementation

--- app/src/main/java/com/example/tomorrow_home_app/ui/article/WriteArticleFragment.kt
+++ app/src/main/java/com/example/tomorrow_home_app/ui/article/WriteArticleFragment.kt
@@ -1,24 +1,32 @@
 package com.example.tomorrow_home_app.ui.article
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.tomorrow_home_app.R
 import com.example.tomorrow_home_app.databinding.FragmentWriteBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.UUID
 
 class WriteArticleFragment : Fragment(R.layout.fragment_write) {
     private lateinit var binding : FragmentWriteBinding
 
-    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+    private var selectedUri : Uri? = null
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
         // Callback is invoked after the user selects a media item or closes the
         // photo picker.
         if (uri != null) {
-            Log.d("PhotoPicker", "Selected URI: $uri")
+            selectedUri = uri
+            binding.photoImageView.setImageURI(uri)
         } else {
-            Log.d("PhotoPicker", "No media selected")
+
         }
     }
 
@@ -26,7 +34,44 @@ class WriteArticleFragment : Fragment(R.layout.fragment_write) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentWriteBinding.bind(view)
 
-        // Launch the photo picker and let the user choose only images.
+
         pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+        binding.photoImageView.setOnClickListener {
+
+        }
+
+        binding.deleteButton.setOnClickListener {
+
+        }
+
+        binding.submitButton.setOnClickListener {
+            if(selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                val fileName = "${UUID.randomUUID()}.png"
+                Firebase.storage.reference.child("articles/photo").child(fileName)
+                    .putFile(photoUri)
+                    .addOnCompleteListener {task ->
+                        if(task.isSuccessful) {
+                            Firebase.storage.reference.child("articles/photo/$fileName")
+                                .downloadUrl
+                                .addOnSuccessListener {
+                                    Log.e("aa" , it.toString())
+                                }.addOnFailureListener {
+
+                                }
+                        }else {
+                            // error Handler
+                            Log.e("bb" , task.exception?.printStackTrace().toString() )
+                        }
+                    }
+            }else {
+                Snackbar.make(view , "이미지가 선택되지 않았습니다." , Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(WriteArticleFragmentDirections.actionBack())
+        }
     }
 }
